ac5b314270cc6f24c57ff009c2e37b96c535785c
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-05-26 11:59:39 +0200

    build the shell on ci as well

--- ci.nix
+++ ci.nix
@@ -1,10 +1,14 @@
 let
   sources = import ./nix/sources.nix;
   pkgs = import ./nix/pkgs.nix { inherit sources; };
-  pre-commit-hooks = import ./nix/pre-commit.nix { inherit sources; };
+  pre-commit = import ./nix/pre-commit.nix { inherit sources; };
 
 in
 {
   release = pkgs.feedbackRelease;
-  pre-commit-check = pre-commit-hooks.run;
+  pre-commit-check = pre-commit.run;
+  shell = pkgs.symlinkJoin {
+    name = "shell";
+    paths = (import ./shell.nix { inherit sources pkgs pre-commit; }).buildInputs;
+  };
 }
