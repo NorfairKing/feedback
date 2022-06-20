2faf08e6eb0de10ea0aa09df4407f6a29646343c
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-06-20 17:20:17 +0200

    Upgrade LTS

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -3,8 +3,11 @@
 import sources.nixpkgs {
   overlays =
     [
-      (import (sources.autodocodec + "/nix/overlay.nix"))
       (final: previous: { inherit (import sources.gitignore { inherit (final) lib; }) gitignoreSource; })
+      (import (sources.autodocodec + "/nix/overlay.nix"))
+      (import (sources.safe-coloured-text + "/nix/overlay.nix"))
+      (import (sources.sydtest + "/nix/overlay.nix"))
+      (import (sources.validity + "/nix/overlay.nix"))
       (import ./overlay.nix)
     ];
   config.allowUnfree = true;
