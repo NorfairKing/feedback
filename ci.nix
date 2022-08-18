0699f6b6263c01e20fcbb76a58d10810aad1a75c
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-08-18 13:52:31 +0200

    build hoogle on ci

--- ci.nix
+++ ci.nix
@@ -7,6 +7,10 @@ in
 {
   release = pkgs.feedbackRelease;
   pre-commit-check = pre-commit.run;
+  hoogle = pkgs.buildEnv {
+    name = "feedback-hoogle";
+    paths = [ (pkgs.haskellPackages.ghcWithHoogle (ps: pkgs.lib.attrValues pkgs.feedbackPackages)) ];
+  };
   shell = pkgs.symlinkJoin {
     name = "shell";
     paths = (import ./shell.nix { inherit sources pkgs pre-commit; }).buildInputs;
