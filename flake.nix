34aceaaad75a90475189696339831d4206d7525c
Jacek Galowicz <jacek@galowicz.de>
2023-05-11 10:53:37 +0200

    Use flake-parts to unlock more architectures

--- flake.nix
+++ flake.nix
@@ -6,89 +6,98 @@
   };
 
   inputs = {
+    autodocodec.flake = false;
+    autodocodec.url = "github:NorfairKing/autodocodec?ref=flake";
+    dekking.flake = false;
+    dekking.url = "github:NorfairKing/dekking";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-22.05";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
-    validity.url = "github:NorfairKing/validity?ref=flake";
-    validity.flake = false;
-    autodocodec.url = "github:NorfairKing/autodocodec?ref=flake";
-    autodocodec.flake = false;
-    safe-coloured-text.url = "github:NorfairKing/safe-coloured-text?ref=flake";
     safe-coloured-text.flake = false;
-    sydtest.url = "github:NorfairKing/sydtest?ref=flake";
+    safe-coloured-text.url = "github:NorfairKing/safe-coloured-text?ref=flake";
     sydtest.flake = false;
-    dekking.url = "github:NorfairKing/dekking";
-    dekking.flake = false;
+    sydtest.url = "github:NorfairKing/sydtest?ref=flake";
+    validity.flake = false;
+    validity.url = "github:NorfairKing/validity?ref=flake";
   };
 
   outputs =
     { self
-    , nixpkgs
-    , pre-commit-hooks
-    , validity
-    , safe-coloured-text
-    , sydtest
     , autodocodec
     , dekking
-    }:
-    let
-      system = "x86_64-linux";
-      pkgsFor = nixpkgs: import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-        overlays = [
-          self.overlays.${system}
-          (import (autodocodec + "/nix/overlay.nix"))
-          (import (safe-coloured-text + "/nix/overlay.nix"))
-          (import (sydtest + "/nix/overlay.nix"))
-          (import (validity + "/nix/overlay.nix"))
-          (import (dekking + "/nix/overlay.nix"))
-        ];
+    , flake-parts
+    , nixpkgs
+    , pre-commit-hooks
+    , safe-coloured-text
+    , sydtest
+    , validity
+    }@inputs: flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      flake = {
+        overlays.default = import ./nix/overlay.nix;
       };
-      pkgs = pkgsFor nixpkgs;
-
-    in
-    {
-      overlays.${system} = import ./nix/overlay.nix;
-      packages.${system}.default = pkgs.feedback;
-      checks.${system} = {
-        package = self.packages.${system}.default;
-        shell = self.devShells.${system}.default;
-        coverage-report = pkgs.dekking.makeCoverageReport {
-          name = "test-coverage-report";
-          coverables = [ "feedback" ];
-          coverage = [ "feedback-test-harness" ];
-        };
-        pre-commit = pre-commit-hooks.lib.${system}.run {
-          src = ./.;
-          hooks = {
-            hlint.enable = true;
-            hpack.enable = true;
-            ormolu.enable = true;
-            nixpkgs-fmt.enable = true;
-            nixpkgs-fmt.excludes = [ ".*/default.nix" ];
-            cabal2nix.enable = true;
+      perSystem = { config, pkgs, lib, system, ... }:
+        let
+          pkgs' = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+            overlays = [
+              self.overlays.default
+              (import (autodocodec + "/nix/overlay.nix"))
+              (import (safe-coloured-text + "/nix/overlay.nix"))
+              (import (sydtest + "/nix/overlay.nix"))
+              (import (validity + "/nix/overlay.nix"))
+              (import (dekking + "/nix/overlay.nix"))
+            ];
+          };
+        in
+        {
+          packages.default = pkgs'.feedback;
+          checks = {
+            package = config.packages.default;
+            shell = config.devShells.default;
+            coverage-report = pkgs'.dekking.makeCoverageReport {
+              name = "test-coverage-report";
+              coverables = [ "feedback" ];
+              coverage = [ "feedback-test-harness" ];
+            };
+            pre-commit = pre-commit-hooks.lib.${system}.run {
+              src = ./.;
+              hooks = {
+                hlint.enable = true;
+                hpack.enable = true;
+                ormolu.enable = true;
+                nixpkgs-fmt.enable = true;
+                nixpkgs-fmt.excludes = [ ".*/default.nix" ];
+                cabal2nix.enable = true;
+              };
+            };
+          };
+          devShells.default = pkgs'.haskellPackages.shellFor {
+            name = "feedback-shell";
+            packages = p: [ p.feedback p.feedback-test-harness ];
+            withHoogle = true;
+            doBenchmark = true;
+            buildInputs = (with pkgs'; [
+              feedback
+              niv
+              zlib
+              cabal-install
+            ]) ++ (with pre-commit-hooks.packages.${system};
+              [
+                hlint
+                hpack
+                nixpkgs-fmt
+                ormolu
+                cabal2nix
+              ]);
+            shellHook = self.checks.${system}.pre-commit.shellHook + pkgs'.feedback.shellHook;
           };
         };
-      };
-      devShells.${system}.default = pkgs.haskellPackages.shellFor {
-        name = "feedback-shell";
-        packages = p: [ p.feedback p.feedback-test-harness ];
-        withHoogle = true;
-        doBenchmark = true;
-        buildInputs = (with pkgs; [
-          feedback
-          niv
-          zlib
-          cabal-install
-        ]) ++ (with pre-commit-hooks.packages.${system};
-          [
-            hlint
-            hpack
-            nixpkgs-fmt
-            ormolu
-            cabal2nix
-          ]);
-        shellHook = self.checks.${system}.pre-commit.shellHook + pkgs.feedback.shellHook;
-      };
     };
 }
